a328ff70230f7332faa4d334bbb8ee74ac3fed3a
SigmundurJohansen <nebuchanezher@hotmail.com>
2024-09-26 15:43:17 +0200

    docking

---
+++ src/ui/gui.cpp
@@ -0,0 +1,92 @@
+#include "gui.h"
+#include "../utils/logger.h"
+#include <string>
+
+bool open_palette = false;
+struct SaveState
+{
+	std::string file_name;
+	std::string file_path;
+};
+
+void set_docking_mode()
+{
+	static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
+	ImGuiViewport *viewport = ImGui::GetMainViewport();
+	// ImGui::DockSpaceOverViewport(viewport); // enable dockspace on main window
+	ImGui::SetNextWindowPos(viewport->Pos);
+	ImGui::SetNextWindowSize(viewport->Size);
+	ImGui::SetNextWindowViewport(viewport->ID);
+	ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoDecoration;
+
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+	ImGui::SetNextWindowBgAlpha(0.0f); // disable dockspace overlay over the game window
+	ImGui::Begin("DockSpace", NULL, window_flags);
+
+	ImGui::PopStyleVar(3);
+	ImGuiID dockspace_id = ImGui::DockSpace(ImGui::GetID("DockSpace"), ImVec2(0, 0), ImGuiDockNodeFlags_PassthruCentralNode);
+
+	static auto first_time = true;
+	if (first_time)
+	{
+		first_time = false;
+		ImGui::DockBuilderRemoveNode(dockspace_id);
+		ImGui::DockBuilderAddNode(dockspace_id, dockspace_flags | ImGuiDockNodeFlags_DockSpace);
+		ImGui::DockBuilderSetNodeSize(dockspace_id, viewport->Size);
+
+		auto dock_id_left = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Left, 0.75f, nullptr, &dockspace_id);
+		auto dock_id_right = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Right, 1.0f, nullptr, &dockspace_id);
+
+		auto dock_id_bottom = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Down, 0.28f, nullptr, &dock_id_left);
+		ImGuiDockNodeFlags dock_flags = ImGuiDockNodeFlags_None;
+		dock_flags |= ImGuiDockNodeFlags_CentralNode;
+		ImGui::DockBuilderDockWindow("Sidebar", dock_id_right);
+		ImGui::DockBuilderDockWindow("Log", dock_id_bottom);
+		ImGui::DockBuilderFinish(dockspace_id);
+	}
+}
+
+void set_render_stats()
+{
+	ImGuiIO &io = ImGui::GetIO();
+	ImGui::Text("Application average %.3f ms/frame", 1000.0f / io.Framerate);
+	ImGui::Text("(%.1f FPS)", io.Framerate);
+}
+
+void list_saved_states(bool &_show)
+{
+	static char loadName[64] = "saveName1";
+	ImGui::InputTextMultiline(" ", loadName, sizeof(loadName), ImVec2(100, 25));
+	ImGui::SameLine();
+	if (ImGui::Button("load"))
+	{
+		_show = false;
+	}
+	ImGui::Separator();
+	ImGui::Text("List of saved states");
+	ImGui::Separator();
+}
+
+void show_app_log(bool *p_open)
+{
+	auto &app = logger::get_instance();
+	ImGui::SetNextWindowSize(ImVec2(500, 400), ImGuiCond_FirstUseEver);
+	// Actually call in the regular Log helper (which will Begin() into the same window as we just did)
+	app.draw("Log", p_open);
+}
+
+static void help_marker(const char *desc)
+{
+	ImGui::TextDisabled("(?)");
+	if (ImGui::IsItemHovered(ImGuiHoveredFlags_DelayShort))
+	{
+		ImGui::BeginTooltip();
+		ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+		ImGui::TextUnformatted(desc);
+		ImGui::PopTextWrapPos();
+		ImGui::EndTooltip();
+	}
+}
+
--- src/ui/window.cpp
+++ src/ui/window.cpp
@@ -1,24 +1,37 @@
 #include "window.h"
+#include "../utils/logger.h"
+#include "gui.h"
+#include "themes.h"
 #include <iostream>
 
+
+static bool b_wireframe = false;
+static bool b_wireframeToggle = false;
+static bool b_show_app_log = true;
+static bool b_show_demo_window = false;
+static bool b_show_demo_windowToggle = false;
+
+bool show_save_menu = false;
+bool show_load_menu = false;
+
 // Error callback function for GLFW
-void glfwErrorCallback(int error, const char* description)
+void glfwErrorCallback(int error, const char *description)
 {
-    std::cerr << "GLFW Error: " << description << std::endl;
+	std::cerr << "GLFW Error: " << description << std::endl;
 }
 
 int window::init()
 {
-    glfwSetErrorCallback(glfwErrorCallback);
+	glfwSetErrorCallback(glfwErrorCallback);
 	if (!glfwInit())
 	{
 		fprintf(stderr, "Failed to initialize GLFW\n");
 		return -1;
 	}
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Setup window
 	m_window = glfwCreateWindow(1200, 700, "template", NULL, NULL);
@@ -31,25 +44,69 @@ int window::init()
 	glfwMakeContextCurrent(m_window);
 	glfwSwapInterval(1); // Enable vsync
 
-    // Initialize GLAD (or GLEW)
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cerr << "Failed to initialize OpenGL loader" << std::endl;
-        return -1;
-    }
-	
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+	// Initialize GLAD (or GLEW)
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "Failed to initialize OpenGL loader" << std::endl;
+		return -1;
+	}
 
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO &io = ImGui::GetIO();
+	(void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;	  // Enable Docking
+	// io.ConfigFlags |= ImGuiViewportFlags_TopMost;         //
+	io.ConfigViewportsNoAutoMerge = true;
+	io.ConfigViewportsNoTaskBarIcon = true;
 
-    // Initialize ImGui for GLFW and OpenGL3 (IMPORTANT)
-    ImGui_ImplGlfw_InitForOpenGL(m_window, true);   // Initialize for GLFW
-    ImGui_ImplOpenGL3_Init("#version 130");       // Initialize for OpenGL3 with GLSL version
-    return 0;
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	embrace_the_darkness();
+	set_to_mouse_style();
+
+	ImGuiStyle &style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_ChildBg].w = 0.0f;
+	}
+	// Initialize ImGui for GLFW and OpenGL3 (IMPORTANT)
+	ImGui_ImplGlfw_InitForOpenGL(m_window, true); // Initialize for GLFW
+	ImGui_ImplOpenGL3_Init("#version 130");		  // Initialize for OpenGL3 with GLSL version
+
+	return 0;
+}
+
+void SetDemoMode()
+{
+	if (b_show_demo_window != b_show_demo_windowToggle)
+	{
+		if (b_show_demo_window)
+			b_show_demo_windowToggle = true;
+		else
+			b_show_demo_windowToggle = false;
+	}
+}
+
+void SetWireframeMode()
+{
+	if (b_wireframe != b_wireframeToggle)
+	{
+		if (b_wireframe)
+		{
+			b_wireframeToggle = true;
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		}
+		else
+		{
+			b_wireframeToggle = false;
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		}
+	}
 }
 
 void window::render()
@@ -63,15 +120,90 @@ void window::render()
 	ImGui_ImplGlfw_NewFrame();
 	ImGui::NewFrame();
 
-	// ImGui window code
-	ImGui::Begin("Hello, world!");
-	ImGui::Text("This is a sample text in ImGui.");
-	ImGui::End();
+	set_docking_mode(); // needs one ImGui::End();
 
-	// Rendering
+	if (b_show_demo_window)
+		ImGui::ShowDemoWindow(&b_show_demo_window);
+
+	ImGuiWindowFlags sidebar_window_flags = 0;
+	ImGui::SetNextWindowBgAlpha(0.0f);					 // disable dockspace overlay over the game window
+	ImGui::Begin("Sidebar", NULL, sidebar_window_flags); // needs one ImGui::End();
+	ImGui::Separator();
+
+	// OPTIONS
+
+	if (ImGui::CollapsingHeader("Options"))
+	{
+		if (ImGui::BeginTable("split", 3))
+		{
+			ImGui::TableNextColumn();
+			ImGui::Checkbox("Wireframe", &b_wireframe);
+			ImGui::TableNextColumn();
+			ImGui::Checkbox("Log", &b_show_app_log);
+			ImGui::TableNextColumn();
+			ImGui::Checkbox("Demo", &b_show_demo_window);
+			ImGui::EndTable();
+		}
+	}
+
+	SetWireframeMode();
+	SetDemoMode();
+
+	if (b_show_app_log)
+		show_app_log(&b_show_app_log);
+
+	ImGui::Separator();
+
+	// Render stats
+	set_render_stats();
+	ImGui::Separator();
+
+	if (ImGui::Button("Save Map"))
+	{
+		show_save_menu = true;
+	}
+
+	ImGuiWindowFlags window_flags = 0;
+	window_flags |= ImGuiWindowFlags_NoCollapse;
+	window_flags |= ImGuiWindowFlags_NoNav;
+	window_flags |= ImGuiWindowFlags_NoDocking;
+
+	if (show_save_menu)
+	{
+		const ImGuiViewport *main_viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + 650, main_viewport->WorkPos.y + 20), ImGuiCond_FirstUseEver);
+		ImGui::Begin("Save Window", &show_save_menu, window_flags);
+		static char saveName[64] = "saveName1";
+		ImGui::InputText("name", saveName, sizeof(saveName));
+		if (ImGui::Button("save entities"))
+		{
+			show_save_menu = false;
+		}
+
+		ImGui::End();
+	}
+	ImGui::SameLine();
+	if (ImGui::Button("Load Map"))
+	{
+		show_load_menu = true;
+	}
+	if (show_load_menu)
+	{
+		const ImGuiViewport *main_viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + 650, main_viewport->WorkPos.y + 20), ImGuiCond_FirstUseEver);
+		ImGui::Begin("Load Window", &show_load_menu, window_flags);
+		list_saved_states(show_load_menu);
+		ImGui::End();
+	}
+
+	ImGui::End();
+	ImGui::End();
 	ImGui::Render();
-	glClear(GL_COLOR_BUFFER_BIT);							// Clear screen
+
+	glClear(GL_COLOR_BUFFER_BIT); // Clear screen
+	// Rendering
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData()); // Render ImGui
+
 	// Swap buffers
 	glfwSwapBuffers(m_window);
 }
